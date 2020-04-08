7db40acee37dd269a783822e28a3c81ccbeca869
Bowie <bowie@thinkful.com>
2020-04-08 10:48:19 -0700

    fix api url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    API_ENDPOINT: "https://warm-anchorage-60574.herokuapp.com/",
+    API_ENDPOINT: "https://warm-anchorage-60574.herokuapp.com",
     API_KEY: process.env.REACT_APP_API_KEY,
     TOKEN_KEY: '6%7AcgLG"2oBiR76%7AcgLG"2oBiR7DTj43Dwq8b'
 }
