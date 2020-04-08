e6db2a88fc540c5076154b3d8f7fa453942ccd08
Bowie <bowie@thinkful.com>
2020-04-07 19:50:11 -0700

    change API_URL

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    API_ENDPOINT: "http://localhost:8000/api",
-    API_KEY: '20bfa7b8-38b4-11ea-a137-2e728ce88125',
+    API_ENDPOINT: "https://warm-anchorage-60574.herokuapp.com/",
+    API_KEY: process.env.REACT_APP_API_KEY,
     TOKEN_KEY: '6%7AcgLG"2oBiR76%7AcgLG"2oBiR7DTj43Dwq8b'
 }
